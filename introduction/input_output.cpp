ba5b7fb7357fc19fb9690ff4551c400a05ae8e11
Temidola <ayoolatemidola@gmail.com>
2022-05-29 23:25:36 -0400

    Add input output

--- introduction/input_output.cpp
+++ introduction/input_output.cpp
@@ -9,13 +9,22 @@ int main(){
 
     std::cerr<< "error message: error!" <<std::endl;
     std::clog << "log message: something happened" << std::endl;
-
+    /*
     int age1;
     std::string name;
-    std::cout << "please type your name and age: " << std::1endl;
-    std::cin >> name;
-    std::cin >> age1;
+    std::cout << "please type your name and age: " << std::endl;
+    std::cin >> name >> age1;
 
-    std::cout << "Hello " << name << "you are " << age1;
+    std::cout << "Hello " << name << ", you are " << age1;
+    */
+
+    std::string full_name;
+    int age2;
+    std::cout << "what is your name and your age? " << std::endl;
+    //fixing the space problem
+    std::getline(std::cin, full_name);
+    std::cin >>age2;
+    std::cout << "Hello " << full_name << ",you are " << age2 << std::endl;
+
     return 0;
 }
